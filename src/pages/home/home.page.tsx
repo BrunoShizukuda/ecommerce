ecbbc1d395210d1a14bf5cd49ec75e709d101e1b
Bruno Shizukuda <shizukudabruno@gmail.com>
2025-01-08 12:09:09 -0300

    add category

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -2,11 +2,13 @@ import React from "react"
 
 //components 
 import Header from "../../components/header/header.components";
+import Categories from "../../components/header/categories/categories.components";
 
 const HomePage = () => {
     return (
         <div>
            <Header/>;   
+           <Categories/>;
         </div>
     )
 }
